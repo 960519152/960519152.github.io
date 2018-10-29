349c95f4d6f4bae2ae9ba89567c819f4ee7a7806
960519152 <byf_06020925@163.com>
2018-10-29 09:33:53 +0800

    根据类型，显示不同提示信息

--- CHECKFORM/10-18表单校验/js/check.js
+++ CHECKFORM/10-18表单校验/js/check.js
@@ -1,3 +1,4 @@
+var flagnumber = 0; //全局判断标识
 function Check(input, errorMsg) {
     this.init = function () {
         this.hide();
@@ -7,49 +8,52 @@ function Check(input, errorMsg) {
     this.hide = function () {//初始化全部隐藏
         $("." + errorMsg).css({ "opacity": 0 });
     };
-    this.eve = function () {//光标移入移出
-        $("." + input).focus(function () {
-            //
-        });
-        $("." + input).blur(function () {
-            var _val = $(this).val();
-            if (!_val) {  //没有输入值
-                $(this).next().css({ "opacity": 1 });
-                console.log($(this).next().attr("data-name") + "栏目输入有误");
-            } else {  //输入值
-                var type = $(this).attr("data-type");
-                switch (type) {
-                    case "text":
-                        console.log("text类型");
-                        break;
-                    case "sex":
-                        console.log("sex类型");
-                        break;
-                    case "time":
-                        console.log("time类型");
-                        break;
-                    case "tel":
-                        console.log("tel类型");
-                        break;
-                    default:
-                        break;
-                }
-                $(this).next().css({ "opacity": 0 });
+    this.test = function (thisinfo) {
+        var _val = $(thisinfo).val(); //输入的值
+        var datatype = $(thisinfo).next().attr("data-name"); //获取需要提示的关键词
+
+        console.log(_val);
+        if (!_val) {  //没有输入值
+            flagnumber++;
+            $(thisinfo).next().css({ "opacity": 1 }).html(datatype + "不能为空");
+        } else {  //输入值验证
+            var type = $(thisinfo).attr("data-type");
+            $(thisinfo).next().html(datatype + "输入有误");
+            switch (type) {
+                case "text":
+                    $(thisinfo).next().css({ "opacity": 0 });
+                    break;
+                case "tel":
+                    var zzTel = /^1(3|4|5|7|8)\d{9}$/;
+                    if (!(zzTel.test(_val))) {
+                        $(thisinfo).next().css({ "opacity": 1 });
+                        flagnumber++;
+                    } else {
+                        $(thisinfo).next().css({ "opacity": 0 });
+                    }
+                    break;
+                default:
+                    break;
             }
-        });
-    }
-    this.okBt = function () {
-        $(".okBt").click(function () {
-            var bool = true;
-            $("." + input).each(function () {
-                var value = $(this).val(); //这里的value就是每一个input的value值~
-                if (!value) {
-                    $(this).next().css({ "opacity": 1 });
-                    bool = false;
-                }
+        }
+    },
+        this.eve = function () {//光标移入移出
+            var thisTest = this.test;//获取test函数对象
+            $("." + input).blur(function () {
+                thisTest(this);
             });
-            if (bool == true) {
-                console.log("验证通过!");
+        }
+    this.okBt = function () {
+        var thisTest = this.test;//获取test函数对象
+        $(".okBt").click(function () {
+            flagnumber = 0; //每点击一次就初始化一次
+            $("." + input).each(function () {
+                thisTest(this);
+            });
+            if (flagnumber == 0) {
+                alert("验证通过!");
+            } else {
+                alert("请检查输入！");
             }
         })
     }
